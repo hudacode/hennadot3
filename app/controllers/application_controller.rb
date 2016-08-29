f8fb2a35dc7b96a4fd6cc9f09811917fa47aa28a
Tom Dallimore <me@tomdallimore.com>
2016-08-29 12:22:36 +0700

    Added location fields to new registration form.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 	protected
 
 	def configure_permitted_parameters
-	  devise_parameter_sanitizer.for(:sign_up) << :name
+	  devise_parameter_sanitizer.for(:sign_up) << [:name, :city, :country]
 	  devise_parameter_sanitizer.for(:account_update) << [:name, :city, :country]
 	end
 end

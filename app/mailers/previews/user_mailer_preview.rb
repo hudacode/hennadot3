c3e293d65205c00da61057b0813e7d78f7468470
Tom Dallimore <me@tomdallimore.com>
2016-09-03 18:56:13 +0700

    Finished mailer preview and setting up background processor.

--- app/mailers/previews/user_mailer_preview.rb
+++ app/mailers/previews/user_mailer_preview.rb
@@ -1,3 +1,12 @@
 class UserMailerPreview < ActionMailer::Preview
 
+    def welcome_email
+        UserMailer.welcome_email(mock_user)
+    end
+
+    private
+
+    def mock_user
+        User.first
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :registrations => "users/registrations" }
-  resources :posts do
-  	member do
-  		get "like", to: "posts#upvote"
-  		get "dislike", to: "posts#downvote"
-  	end
-  	resources :comments
-	end
+    devise_for :users, :controllers => { :registrations => "users/registrations" }
+        resources :posts do
+      	    member do
+      		      get "like", to: "posts#upvote"
+      		      get "dislike", to: "posts#downvote"
+      	    end
+      	resources :comments
+	  end
 
-  root 'posts#index'
-  get '/about', to: 'pages#about'
-
+    root 'posts#index'
+    get '/about', to: 'pages#about'
+    mount Sidekiq::Web => '/wtfitssidekiq'
 end
